ee858693111a015faa537581727d5cf6c1ee2580
Alexandre Beranger <alexandre.beranger54@gmail.com>
2023-08-27 23:23:46 +0200

    - Ajout d'un système de tri dans une table

--- src/component/DataTable.js
+++ src/component/DataTable.js
@@ -1,21 +1,36 @@
 import DataTableRow from "./DataTableRow";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function DataTable(props) {
+
+    const dispatch = useDispatch();
+    var dataSort = useSelector((state) => state.sorted);
+    if(!dataSort.length){
+        dataSort = props.data;
+    }
 
     const head = [];
     const body = [];
 
     props.columns.forEach((e, i) => {
-        head.push(<th key={"head" + i}>{e.title}</th>);
+        head.push(<th onClick={() => sort(e.data)} key={"head" + i}>{e.title}</th>);
     });
 
-    props.data.forEach((e, i) =>{
+    dataSort.forEach((e, i) =>{
         const cols = [];
         props.columns.forEach(col => {
             cols.push(e[col.data]);
         })
         body.push(<DataTableRow key={"row" + i} data={cols} />)
     })
+
+    function sort(colID){
+        props.data.sort((a, b) => {
+            return a[colID] > b[colID];
+        });
+        dispatch({type: "setSortedList", payload: {sorted: dataSort}});
+    }
 
     return (
         <table>
--- src/store.js
+++ src/store.js
@@ -2,13 +2,60 @@ import { createStore } from 'redux';
 
 const initialState = {
     modalOpen: false,
-    employees: []
+    sorted: [],
+    employees: [
+        {
+            city: "B",
+            dateOfBirth: "DateOfBirth",
+            department: "Department",
+            firstName: "FirstName",
+            lastName: "LastName",
+            startDate: "22/04/1999",
+            state: "State",
+            street: "Street",
+            zipCode: "1"
+        },
+        {
+            city: "A",
+            dateOfBirth: "DateOfBirth",
+            department: "Department",
+            firstName: "FirstName",
+            lastName: "LastName",
+            startDate: "22/04/2001",
+            state: "State",
+            street: "Street",
+            zipCode: "3"
+        },
+        {
+            city: "C",
+            dateOfBirth: "DateOfBirth",
+            department: "Department",
+            firstName: "FirstName",
+            lastName: "LastName",
+            startDate: "22/04/2000",
+            state: "State",
+            street: "Street",
+            zipCode: "2"
+        },
+        // {
+        //     city: "City",
+        //     dateOfBirth: "DateOfBirth",
+        //     department: "Department",
+        //     firstName: "FirstName",
+        //     lastName: "LastName",
+        //     startDate: "StartDate",
+        //     state: "State",
+        //     street: "Street",
+        //     zipCode: "ZipCode"
+        // }
+    ]
 };
 
 function reducer(state = initialState, action){
     switch(action.type){
         case "switchModal": return {...state, modalOpen: action.payload.open};
         case "addEmployee": return {...state, employees: [...state.employees, action.payload.employee]};
+        case "setSortedList": return {...state, sorted: action.payload.sorted};
         case "clear": return {...initialState}; //DEBUG
         default: return state;
     }
